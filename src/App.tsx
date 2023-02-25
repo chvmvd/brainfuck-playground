3045663177cd1eb39c5e3b3c638775ba565cd67e
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-02-25 20:58:32 +0900

    Create brainfuckInterpreter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import reactLogo from "./assets/react.svg";
+import brainfuckInterpreter from "./components/brainfuckInterpreter";
 
 function App() {
+  useEffect(() => {
+    const output = brainfuckInterpreter("++++++++[>++++++++<-]>+.+.+.", "");
+    console.log(output);
+  }, []);
   return (
     <>
       <div>Hello World!</div>
---
+++ src/components/brainfuckInterpreter.ts
@@ -0,0 +1,90 @@
+type Storage = number[];
+type BrainfuckCommand = "<" | ">" | "+" | "-" | "." | "," | "[" | "]";
+type BrainfuckCode = string;
+
+function getNextCloseParenIndex(sourceCode: string, codePointer: number) {
+  let closeParenCounter = 0;
+  codePointer++;
+  while (closeParenCounter < 1) {
+    if (sourceCode[codePointer] === "[") {
+      closeParenCounter--;
+    }
+    if (sourceCode[codePointer] === "]") {
+      closeParenCounter++;
+    }
+    codePointer++;
+  }
+  return codePointer - 1;
+}
+
+function getPreviousOpenParenIndex(sourceCode: string, codePointer: number) {
+  let openParenCounter = 0;
+  codePointer--;
+  while (openParenCounter < 1) {
+    if (sourceCode[codePointer] === "]") {
+      openParenCounter--;
+    }
+    if (sourceCode[codePointer] === "[") {
+      openParenCounter++;
+    }
+    codePointer--;
+  }
+  return codePointer + 1;
+}
+
+export default function brainfuckInterpreter(
+  sourceCode: BrainfuckCode,
+  input: string
+) {
+  const storage: Storage = [...Array(1000000)].map((_) => 0);
+  let pointer = 0;
+  let codePointer = 0;
+  let output = "";
+  while (codePointer < sourceCode.length) {
+    const command: BrainfuckCommand = sourceCode[
+      codePointer
+    ] as BrainfuckCommand;
+    switch (command) {
+      case ">":
+        pointer++;
+        codePointer++;
+        break;
+      case "<":
+        pointer--;
+        codePointer++;
+        break;
+      case "+":
+        storage[pointer]++;
+        codePointer++;
+        break;
+      case "-":
+        storage[pointer]--;
+        codePointer++;
+        break;
+      case ".":
+        output = `${output}${String.fromCharCode(storage[pointer])}`;
+        codePointer++;
+        break;
+      case ",":
+        storage[pointer] = input.charCodeAt(0);
+        input = input.slice(1);
+        codePointer++;
+        break;
+      case "[":
+        if (storage[pointer] === 0) {
+          codePointer = getNextCloseParenIndex(sourceCode, codePointer) + 1;
+        } else {
+          codePointer++;
+        }
+        break;
+      case "]":
+        if (storage[pointer] !== 0) {
+          codePointer = getPreviousOpenParenIndex(sourceCode, codePointer) + 1;
+        } else {
+          codePointer++;
+        }
+        break;
+    }
+  }
+  return output;
+}
