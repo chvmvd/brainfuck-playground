89dae761d84998f28f1eb30885f14cc65ad73cff
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-02-25 20:40:56 +0900

    Run Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import reactLogo from './assets/react.svg'
+import reactLogo from "./assets/react.svg";
 
 function App() {
   return (
     <>
       <div>Hello World!</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
