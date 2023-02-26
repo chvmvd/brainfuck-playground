39266ccab52899b5dd8e2fa3cad1cb96c8b05237
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-02-26 21:53:48 +0900

    Fix bug

--- src/components/brainfuckSyntaxChecker.ts
+++ src/components/brainfuckSyntaxChecker.ts
@@ -2,16 +2,18 @@ import {
   type BrainfuckSyntaxStatus,
   type BrainfuckCode,
 } from "./brainfuckDefinitions";
+import brainfuckParser from "./brainfuckParser";
 
 export default function brainfuckSyntaxChecker(
-  sourceCode: BrainfuckCode
+  sourceCode: string
 ): BrainfuckSyntaxStatus {
-  let openBracketCount = (sourceCode.match(/\[/g) || []).length;
-  let closeBracketCount = (sourceCode.match(/\]/g) || []).length;
+  const parsedSourceCode = brainfuckParser(sourceCode);
+  let openBracketCount = (parsedSourceCode.match(/\[/g) || []).length;
+  let closeBracketCount = (parsedSourceCode.match(/\]/g) || []).length;
   if (openBracketCount > closeBracketCount) return "Too many open bracket!";
   if (openBracketCount < closeBracketCount) return "Too many close bracket!";
   let cnt = 0;
-  for (const char of sourceCode) {
+  for (const char of parsedSourceCode) {
     if (char === "[") cnt++;
     if (char === "]") cnt--;
     if (cnt < 0) return "bracket mismatch!";
